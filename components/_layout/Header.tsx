76ae40d2bf012c9fc4856e655baf714bdb5d4906
scures <scurescu@suse.com>
2022-08-30 09:18:19 +0200

    Menu, responsive

--- components/_layout/Header.tsx
+++ components/_layout/Header.tsx
@@ -1,8 +1,12 @@
-import useStore from '@/store/main.store'
+import { RancherLogoSVG } from '@/assets/images'
+import EpinioLogo from '@/assets/products-logos/icon-epinio.svg'
+import HarvesterLogo from '@/assets/products-logos/icon-harvester.svg'
+import KubewardenLogo from '@/assets/products-logos/icon-kubewarden.svg'
+import OpniLogo from '@/assets/products-logos/icon-opni.svg'
+import useStore, { ThemeI } from '@/store/main.store'
+import Image from 'next/image'
 import { useEffect } from 'react'
 import { DocAndGitLinks } from '../DocAndGitLinks'
-import {ThemeI} from '@/store/main.store'
-
 export const Header = () => {
   const { theme, updateTheme } = useStore((state) => state)
 
@@ -39,14 +43,39 @@ export const Header = () => {
               <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5' />
             </svg>
 
-            <ul className='absolute top-6 hidden group-hover:flex flex-col bg-primary-light dark:bg-secondary-dark shadow-sm  p-4 z-10 min-w-full '>
-              <li>Link 1</li>
-              <li>Link 2</li>
-              <li>Link 3</li>
-            </ul>
+            <div className='absolute -left-20 md:left-0 top-6 hidden group-hover:flex flex-col bg-primary-light bg-opacity-95  dark:bg-secondary-dark text-sm drop-shadow shadow-sm shadow- p-4 z-10 min-w-[180px] transition-all'>
+              <a rel="nofollow noreferrer" href='https://rancher.com/' target="_blank" className='flex gap-2 hover:ml-2 transition-all'>
+                <RancherLogoSVG className='w-4' />
+                Rancher
+              </a>
+
+              <hr className='m-2 border-gray-400 border-opacity-20' />
+
+              <a rel="nofollow noreferrer" href='https://epinio.io/' target="_blank" className='flex py-2 gap-2 hover:ml-2 transition-all'>
+                <Image width={16} height={16} src={EpinioLogo.src} alt='' />
+                Epinio
+              </a>
+              <a rel="nofollow noreferrer" href='https://harvesterhci.io/' target="_blank" className='flex py-2 gap-2 hover:ml-2 transition-all'>
+                <Image width={16} height={16} src={HarvesterLogo.src} alt='' />
+                Harvester
+              </a>
+              <a rel="nofollow noreferrer" href='https://www.kubewarden.io/' target="_blank" className='flex py-2 gap-2 hover:ml-2 transition-all'>
+                <Image width={20} height={20} src={KubewardenLogo.src} alt='' />
+                Kubewarden
+              </a>
+              <a rel="nofollow noreferrer" href='https://opni.io/' target="_blank" className='flex py-2 gap-2 hover:ml-2 transition-all'>
+                <Image width={16} height={16} src={OpniLogo.src} alt='' />
+                Opni
+              </a>
+
+              <hr className='m-2 border-gray-400 border-opacity-20' />
+              <a rel="nofollow noreferrer" href='https://opensource.suse.com/' target="_blank" className='flex gap-2 hover:ml-2 transition-all'>
+                More projects...
+              </a>
+            </div>
           </div>
 
-          <button className='px-2 bg-white rounded-full' onClick={toggleTheme}>
+          <button className='px-2 bg-white rounded-full transition-all' onClick={toggleTheme}>
             <span>{theme === 'dark' ? '🌞' : '🌚'}</span>
           </button>
         </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
