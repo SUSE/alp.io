31967d1557f0a7804f8a75ec68c6d12e590af96a
gmontalvoy <gmontalvoy@gmail.com>
2023-02-09 01:15:27 +0000

    Deploying to gh-pages from @ SUSE/alp.io@38c6d8bd7d3b39b5abc86d12cc1ca7509b98b49e 🚀

--- _next/static/-V-Y3H5D_XVcHJTC0cSQz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/687-5b701e233356f4da.js","static/chunks/pages/index-bb3f0e3f74d4d567.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/akU03i9Q24isNgldu294e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/687-5b701e233356f4da.js","static/chunks/pages/index-ba5fa6b56b8bd348.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
